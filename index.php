cf556f7df1ea389747e61c4d7fa83025325bdbc2
maruf1126 <maruf1126@gmail.com>
2015-05-01 20:17:36 -0400

    user-registrations

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php  //Start the Session
+
+ require('connect.php');
+
+if (isset($_POST['username']) and isset($_POST['password'])){
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
+ 
+$result = mysql_query($query) or die(mysql_error());
+$count = mysql_num_rows($result);
+
+if ($count == 1){
+$_SESSION['username'] = $username;
+}else{
+
+echo "Invalid Login Credentials.";
+}
+}
+
+if (isset($_SESSION['username'])){
+$username = $_SESSION['username'];
+echo "Hai " . $username . "
+";
+echo "This is the Members Area
+";
+echo "<a href='logout.php'>Logout</a>";
+
+}else{
+// When the user visits the page first time, simple login form will be displayed.
+?>
+<!DOCTYPE html>
+<head>
+    <title>CodingCyber - Simple Login Script</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+<!-- Form for logging in the users -->
+
+<div class="register-form">
+    <?php
+	if(isset($msg) & !empty($msg)){
+		echo $msg;
+	}
+ ?>
+    <h1>Login</h1>
+    <form action="" method="POST">
+        <p><label>User Name : </label>
+            <input id="username" type="text" name="username" placeholder="username" /></p>
+
+        <p><label>Password&nbsp;&nbsp; : </label>
+            <input id="password" type="password" name="password" placeholder="password" /></p>
+
+        <a class="btn" href="register.php">Register</a>
+        <input class="btn register" type="submit" name="submit" value="Login" />
+    </form>
+</div>
+<?php } ?>
+</body>
+</html>
